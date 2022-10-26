f9186c58e785b091d4507cd19840205c22ffd50d
ImPierreAtMano <pierre.bernard@manomano.com>
2022-10-26 18:33:34 +0200

    Complément pour l'installation et précision smoke test

--- test/e2e/cypress/e2e/1_smoke_test.js
+++ test/e2e/cypress/e2e/1_smoke_test.js
@@ -4,6 +4,6 @@ When("Je visite la page principale", () => {
   cy.visit('/');
 });
 
-Then("Je devrais voir {string} ENV !", greating => {
-  cy.get("h1").should("contain", greating + Cypress.env('host') + " !")
+Then("Je devrais voir {string} dans le bon environnement !", (greating) => {
+  cy.get("h1").should("contain", greating + Cypress.env("host") + " !")
 });
